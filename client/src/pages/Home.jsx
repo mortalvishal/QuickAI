94ab94726761be1490a0b35c7b2079f26be9fb44
vishal pathak <vishalpathak842@gmail.com>
2025-08-04 23:26:52 +0530

    Add Navbar component to Home page for improved navigation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <>
+       <Navbar/>
+    </>
   )
 }
