29ecff0ddbaeb8627aad2fce8349e66cf6a5a94f
vishal pathak <vishalpathak842@gmail.com>
2025-08-23 01:23:46 +0530

    Add Testimonial component to Home page for enhanced user engagement

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AITools from '../components/AITools'
+import Testimonial from '../components/testimonial'
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
        <Navbar/>
        <Hero/>
        <AITools/>
+       <Testimonial/>
     </>
   )
 }
