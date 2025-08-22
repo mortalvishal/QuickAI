ffef08b8d65030364df0661b1ddc61850348aecb
vishal pathak <vishalpathak842@gmail.com>
2025-08-23 00:54:20 +0530

    Add Hero and AITools components to Home page for enhanced user experience

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AITools from '../components/AITools'
 
 export const Home = () => {
   return (
     <>
        <Navbar/>
+       <Hero/>
+       <AITools/>
     </>
   )
 }
